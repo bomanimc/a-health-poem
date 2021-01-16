874c6ac37ded2a38ccdb5eecb274501ae4816396
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-01-16 10:20:15 -0500

    Layout the home page

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,14 +2,66 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import styled, { createGlobalStyle } from 'styled-components';
+import '@fontsource/cormorant-garamond';
 
 const GlobalStyle = createGlobalStyle`
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed, 
+  figure, figcaption, footer, header, hgroup, 
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+
+  article, aside, details, figcaption, figure, 
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+
+  body {
+    line-height: 1;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  blockquote, q {
+    quotes: none;
+  }
+
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
   html, body, #___gatsby, #gatsby-focus-wrapper {
     padding: 0;
     margin: 0;
     height: 100%;
+    color: #444444;
+    font-family: 'Cormorant Garamond', serif;
   }
-`
+`;
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -45,15 +97,16 @@ Layout.Root = styled.div`
 
 Layout.Main = styled.main`
   flex: 1;
+  display: flex;
 `;
 
 Layout.Footer = styled.footer`
   display: flex;
   justify-content: center;
   align-items: center;
-  color: #444444;
   opacity: 0.5;
   font-style: italic;
+  margin: 2rem;
   
   a {
     text-decoration: none;
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,51 @@ import SEO from "../components/seo";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <IndexPage.Content>
+      <IndexPage.Title>Restatement</IndexPage.Title>
+      <IndexPage.Quote>
+        “The repetition of words and concepts can change their implications, just
+        as the physical act of repeating a word aloud can alter its meaning” – 
+        Making Sense: Decoding Gertrude Stein
+      </IndexPage.Quote>
+      <IndexPage.ContributeButton type="button">Contribute</IndexPage.ContributeButton>
+    </IndexPage.Content>
   </Layout>
 );
 
+IndexPage.Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  max-width: 75rem;
+  margin: 0 auto;
+`;
+
+IndexPage.Title = styled.h1`
+  font-size: 9rem;
+  margin-top: 9rem;
+  margin-bottom: 2rem;
+  
+`;
+
+IndexPage.Quote = styled.h5`
+  font-size: 2.25rem;
+  text-align: center;
+  font-weight: 300;
+`;
+
+IndexPage.ContributeButton = styled.button`
+  border: 1px solid #444444;
+  border-radius: 0.5rem;
+  margin-top: 5rem;
+  padding: 0 5rem;
+  height: 4rem;
+  background: none;
+  font-size: 1.5rem;
+  color: #444444;
+  font-family: 'Cormorant Garamond';
+`;
 
 export default IndexPage;
