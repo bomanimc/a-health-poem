978edb689e7f2cb8513d39277e4f38298e4bd8ee
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-01-17 08:56:19 -0500

    Add input design

--- src/pages/contribute.js
+++ src/pages/contribute.js
@@ -1,22 +1,40 @@
 import React from "react";
 import styled from 'styled-components';
-import { Link } from 'gatsby';
+import RightArrow from "../assets/right-arrow.svg";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const ContributePage = () => (
-  <Layout>
-    <SEO title="Contribute" />
-    <ContributePage.Content>
-      <ContributePage.LineNumber>line 3</ContributePage.LineNumber>
-      <ContributePage.LineText>
-        ideas recur, although the ideas may be the same, we see them in different relationships
-      </ContributePage.LineText>
-      <ContributePage.Divider />
-    </ContributePage.Content>
-  </Layout>
-);
+const ContributePage = () => {
+  const lineNumber = 3;
+
+  return (
+    <Layout>
+      <SEO title="Contribute" />
+      <ContributePage.Content>
+        <ContributePage.LineNumber>line {lineNumber}</ContributePage.LineNumber>
+        <ContributePage.LineText>
+          ideas recur, although the ideas may be the same, we see them in different relationships
+        </ContributePage.LineText>
+        <ContributePage.Divider />
+        <ContributePage.Form>
+          <ContributePage.InputContainer>
+            <ContributePage.Input
+              input type="text"
+              placeholder="Enter your contribution here" 
+            />
+            <ContributePage.Submit>
+              <RightArrow />
+            </ContributePage.Submit>
+          </ContributePage.InputContainer>
+          <ContributePage.InstructionalDetail>
+            Restate line {lineNumber} in your own words.
+          </ContributePage.InstructionalDetail>
+        </ContributePage.Form>
+      </ContributePage.Content>
+    </Layout>
+  );
+};
 
 ContributePage.Content = styled.div`
   display: flex;
@@ -46,6 +64,54 @@ ContributePage.Divider = styled.hr`
   opacity: 0.5;
   width: 15rem;
   margin: 4rem 0;
+`;
+
+ContributePage.Form = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+ContributePage.InputContainer = styled.div`
+  flex: 1;
+  position: relative;
+  border-bottom: 1px solid rgba(68, 68, 68, 0.5);
+  padding-right: 4rem;
+`;
+
+ContributePage.Input = styled.input`
+  width: 100%;
+  font-size: 2.25rem;
+  color: #444444;
+  font-family: 'Cormorant Garamond', serif;
+  font-weight: 500;
+  padding: 0.5rem 0 0.5rem 0.5rem;
+  border: 0;
+`;
+
+ContributePage.Submit = styled.button`
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translateY(-50%);
+  background: 0;
+  border: none;
+  padding: 0;
+
+  input[type="button"] {
+    display: none;
+  }
+
+  svg {
+    width: 2rem;
+    fill: #444444;
+  }
+`;
+
+ContributePage.InstructionalDetail = styled.p`
+  font-size: 1.5rem;
+  margin-top: 1rem;
+  margin-left: 0.5rem;
 `;
 
 export default ContributePage;
