aa0a3b0a3874c350eab5346dbf76d3d1f5dea8ee
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-05-04 07:49:44 -0400

    Improve mobile home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,12 +9,10 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <IndexPage.Content>
-      <IndexPage.Title>Restatement</IndexPage.Title>
-      <IndexPage.Quote>
-        “The repetition of words and concepts can change their implications, just
-        as the physical act of repeating a word aloud can alter its meaning” – 
-        Making Sense: Decoding Gertrude Stein
-      </IndexPage.Quote>
+      <IndexPage.Title>A Health Poem</IndexPage.Title>
+      <IndexPage.Subtitle>
+        A collaborative poem exploring the meaning of health, in your own words. 
+      </IndexPage.Subtitle>
       <IndexPage.ContributeLink to="/contribute">Contribute</IndexPage.ContributeLink>
     </IndexPage.Content>
   </Layout>
@@ -28,19 +26,32 @@ IndexPage.Content = styled.div`
   flex: 1;
   max-width: 75rem;
   margin: 0 auto;
+
+  @media (max-width: 896px) {
+    margin: 0 1rem;
+  }
 `;
 
 IndexPage.Title = styled.h1`
   font-size: 9rem;
   margin-top: 9rem;
   margin-bottom: 2rem;
-  
+  text-align: center;
+
+  @media (max-width: 896px) {
+    font-size: 6rem;
+    margin-top: 3rem;
+  }
 `;
 
-IndexPage.Quote = styled.h5`
+IndexPage.Subtitle = styled.h5`
   font-size: 2.25rem;
   text-align: center;
   font-weight: 300;
+
+  @media (max-width: 896px) {
+    font-size: 1.5rem;
+  }
 `;
 
 IndexPage.ContributeLink = styled(Link)`
@@ -48,7 +59,7 @@ IndexPage.ContributeLink = styled(Link)`
   border-radius: 0.5rem;
   margin-top: 5rem;
   padding: 0 5rem;
-  height: 4rem;
+  height: 3rem;
   font-size: 1.5rem;
   color: #444444;
   font-family: 'Cormorant Garamond';
