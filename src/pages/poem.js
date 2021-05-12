df76815e20d6111f6c4158f09af698dab56fbefd
Bomani Oseni McClendon <bomanimc@gmail.com>
2021-05-11 21:26:02 -0400

    Improve Poem component naming

--- src/pages/poem.js
+++ src/pages/poem.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const AffirmationPage = () => {
+const Poem = () => {
   const [contributions, setContributions] = useState([]);
 
   useEffect(() => {
@@ -16,54 +16,81 @@ const AffirmationPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
-      <AffirmationPage.Content>
-        <AffirmationPage.Title>A Health Poem</AffirmationPage.Title>
-        <AffirmationPage.Divider />
-        <AffirmationPage.Lines>
-          {contributions.map(contribution => <AffirmationPage.Line key={contribution.contribution}>{contribution.contribution}</AffirmationPage.Line>)}
-        </AffirmationPage.Lines>
-      </AffirmationPage.Content>
+      <Poem.Content>
+        <Poem.ContentContainer>
+          <Poem.PoemContainer>
+            <Poem.Title>A Health Poem</Poem.Title>
+            <Poem.Subtitle>A collaborative poem exploring the meaning of health.</Poem.Subtitle>
+            <Poem.Lines>
+
+              {contributions.map(contribution =>
+                <Poem.Line key={contribution.contribution}>
+                  {contribution.contribution}
+                </Poem.Line>
+              )}
+            </Poem.Lines>
+          </Poem.PoemContainer>
+        </Poem.ContentContainer>
+      </Poem.Content>
     </Layout>
   );
 };
 
-AffirmationPage.Content = styled.div`
+Poem.Content = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   flex: 1;
-  max-width: 75rem;
-  margin: 0 auto;
+  height: 80vh;
+  padding: 2rem;
 `;
 
-AffirmationPage.Title = styled.h3`
-  font-size: 4rem;
+Poem.ContentContainer = styled.div`
+  max-width: 50rem;
+  margin: 2rem auto;
+  height: 100%;
+  width: 100%;
+`;
+
+Poem.PoemContainer = styled.div`
+  border: 1px solid #E0E0E0;
+  padding: 1rem;
+  overflow-y: scroll;
+  box-shadow: 10px 5px 5px #E0E0E0;
+  height: 100%;
   font-style: italic;
 `;
 
-AffirmationPage.Divider = styled.hr`
+Poem.Title = styled.h3`
+  font-size: 4rem;
+`;
+
+Poem.Subtitle = styled.p`
+  font-size: 1rem;
+  margin-bottom: 2rem; 
+`;
+
+Poem.Divider = styled.hr`
   width: 15rem;
   margin: 1.5rem 0 3rem 0;
   border: 0.5px solid #444444;
   opacity: 0.5;
 `;
 
-AffirmationPage.Lines = styled.div`
+Poem.Lines = styled.div`
   font-style: italic;
-  font-size: 2.25rem;
-  text-align: center;
+  font-size: 1.5rem;
 `;
 
-AffirmationPage.Line = styled.p`
-  margin-bottom: 2rem;
+Poem.Line = styled.p`
+  margin-bottom: 1rem;
   font-weight: ${p => p.isOwnedContribution ? 'bold' : 'normal'};
 `;
 
-AffirmationPage.MetaDetail = styled.p`
+Poem.MetaDetail = styled.p`
   margin-top: 4rem;
   margin-bottom: 4rem;
   color: rgba(68, 68, 68, 0.5);
 `;
 
-export default AffirmationPage;
+export default Poem;
